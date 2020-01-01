30bfbeaab9721d6a35ff716eaca53185f459fd67
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-01 23:54:38 +1100

    stuff to go over with bruce

--- pizza_order_version2/index.js
+++ pizza_order_version2/index.js
@@ -1,5 +1,6 @@
 var prompt = require("prompt");
 const getUserName = require("./functions");
+
 /*
 const user = require('./user');
 console.log(`User: ${user.getName()}`);
@@ -10,12 +11,41 @@ console.log(pizzaType);
 
 console.log("Welcome To The Pizza Application.\n Firstly, can i get your name?\n");
 
+// Get process.stdin as the standard input object.
+var standard_input = process.stdin;
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
+// Prompt user to input data in console.
+console.log("Please enter your name.");
+// When user input data and click enter key.
+standard_input.on('data', function (data) {
+    // User input exit.
+    if(data === 'exit\n'){
+        // Program exit.
+        console.log("User input complete, program exit.");
+        process.exit();
+    }else
+    {
+        // Print user input in console.
+        console.log("Hey There " + data);
+        console.log("Are you ready to order?")
+    }
+});
 
-    prompt.start();
-    prompt.get(['name'], function (err, result) {
-    console.log(`Hey There ${result.name}!, Are you ready to order?`);
-    });
-
+var readyOrder = process.stdin;
+readyOrder.setEncoding("utf-8");
+console.log("Please enter YES or NO");
+standard_input.on("data", function (userSelection) {
+ if(userSelection === "No"){
+     console.log("Why did you even show up?");
+     process.exit();
+ }else if(userSelection === "YES"){
+     console.log("Let's get ordering");
+ }else {
+     console.log("try again");
+ };
+ 
+});
 
 
 
